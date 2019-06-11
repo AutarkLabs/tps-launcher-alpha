81bbb0cbb9e8a620206f908f044251e19ca48dcb
Otto G <ottodevs@gmail.com>
2019-06-11 02:14:13 +0200

    Add custom wizard steps to onboarding definitions

--- src/onboarding/templates.js
+++ src/onboarding/templates.js
@@ -2,15 +2,18 @@ import PropTypes from 'prop-types'
 
 import democracy from './templates/democracy'
 import multisig from './templates/multisig'
+import tps from './templates/tps'
 
 const Multisig = Symbol('Multisig')
 const Democracy = Symbol('Democracy')
+const ThatPlanningSuite = Symbol('ThatPlanningSuite')
 
 const Templates = new Map()
 Templates.set(Democracy, democracy)
 Templates.set(Multisig, multisig)
+Templates.set(ThatPlanningSuite, tps)
 
-const TemplateType = PropTypes.oneOf([Multisig, Democracy])
+const TemplateType = PropTypes.oneOf([Multisig, Democracy, ThatPlanningSuite])
 
 export default Templates
-export { TemplateType, Multisig, Democracy }
+export { TemplateType, Multisig, Democracy, ThatPlanningSuite }
